a136b78d1000654f692d1990acd33dfb72c09ab7
Zakariyya Mughal <zaki.mughal@gmail.com>
2015-01-24 15:18:44 -0600

    add Inline 'with' support
    closes <https://github.com/zmughal/p5-Alien-WFDB/issues/2>

--- lib/Alien/WFDB.pm
+++ lib/Alien/WFDB.pm
@@ -5,10 +5,24 @@ use warnings;
 
 use parent 'Alien::Base';
 
+sub Inline {
+	return unless $_[-1] eq 'C'; # Inline's error message is good
+	my $self = __PACKAGE__->new;
+	+{
+		LIBS => $self->libs,
+		INC => $self->cflags,
+		AUTO_INCLUDE => '#include "wfdb.h"',
+	};
+}
+
 1;
 # ABSTRACT: Alien package for the WFDB (WaveForm DataBase) library
 
 =pod
+
+=head1 Inline support
+
+This module supports L<Inline's with functionality|Inline/"Playing 'with' Others">.
 
 =head1 SEE ALSO
 
